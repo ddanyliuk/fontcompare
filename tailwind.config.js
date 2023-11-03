06748c56f9ea7ff476dd6a347dd5ebb5cd49740c
Dmytro Danyliuk <ddanyliuk@eisgroup.com>
2023-11-03 01:08:23 -0400

    - initial commit, update .gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+      '7': '56px',
+      '8': '64px',
+      '9': '120px',
+      '10': '160px',
+      '11': '200px',
+      '72': '288px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
